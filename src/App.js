ee5b17438b33c07e22b3bc70bc2482e207fc5615
Grant Kirill <webdotg.inbox@gmail.com>
2023-05-07 21:15:59 +0300

    pizdec 2 chasa component na element blyat

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      </div>
+      <Routes>
+        <Route index path='/' element={<HomePage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+      </Routes>
+    </div>
   );
 }
 
