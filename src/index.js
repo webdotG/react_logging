f5a4fc3c564b0e64d31812109465143482c98e47
Grant Kirill <webdotg.inbox@gmail.com>
2023-05-09 17:10:43 +0300

    working version before add components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
+import './index.css';
+import App from './App'
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import App from './App';
-
+import { Provider } from 'react-redux';
+import { store } from './store';
+import './firebase';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode >
 );
